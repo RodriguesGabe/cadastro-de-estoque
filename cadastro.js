fab08507dbe85033c190685c82c44f5c4dadd1c5
RodriguesGabe <105085377+RodriguesGabe@users.noreply.github.com>
2022-11-18 10:22:23 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,77 @@
+//-----------------------------------------------------------------------------------------------------------
+// Função: validarProduto(idNomeProduto, idCodProduto, idQtidadeProduto)
+// Verifica se foram informados o nome e o código do produto
+//-----------------------------------------------------------------------------------------------------------
+function validarProduto(idNomeProduto, idCodProduto, idQtidadeProduto) {
+    let nome = document.getElementById(idNomeProduto).value;
+    let codigo = document.getElementById(idCodProduto).value;
+    let qtidade = document.getElementById(idQtidadeProduto).value;
+
+    if (nome == "")
+        alert("Nome do produto não pode estar em branco. Favor preenchê-lo!");
+    else if (codigo == "")
+        alert("Código do produto não pode estar em branco. Favor preenchê-lo!");
+    else cadastrarProduto(nome, codigo, parseInt(qtidade));
+}
+//-----------------------------------------------------------------------------------------------------------
+// Função: cadastrarProduto(produto, codig, qtidade)
+// Cadastra um novo produto (nome, código e quantidade) no estoque
+//-----------------------------------------------------------------------------------------------------------
+function cadastrarProduto(produto, codig, qtidade) {
+    let novoProduto = {nome:produto, codigo:codig, quantidade:qtidade};
+
+    if (typeof(Storage) !== "undefined") {
+        let produtos = localStorage.getItem("produtos");
+        if (produtos == null) produtos = []; // Nenhum produto ainda foi cadastrado
+        else produtos = JSON.parse(produtos);
+        produtos.push(novoProduto); // Adiciona um novo produto
+        localStorage.setItem("produtos",JSON.stringify(produtos))
+        alert("Foram cadastradas com sucesso "+qtidade+" unidades do produto "+ produto+"!");
+        atualizarTotalEstoque("totalEstoque");
+        location.reload();
+    } 
+    else alert("A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação");
+}
+
+//-----------------------------------------------------------------------------------------------------------
+// Função: atualizarTotalEstoque(idCampo)
+// Incrementa a quantidade de itens cadastrado no estoque (carrinho localizado no canto superior da tela)
+//-----------------------------------------------------------------------------------------------------------
+function atualizarTotalEstoque(idCampo) {
+    localStorage.setItem("totalEstoque",++document.getElementById(idCampo).innerHTML)
+}
+//-----------------------------------------------------------------------------------------------------------
+// Função: carregarTotalEstoque(idCampo)
+// Incrementa a quantidade de itens cadastrado no estoque (carrinho localizado no canto superior da tela)
+//-----------------------------------------------------------------------------------------------------------
+function carregarTotalEstoque(idCampo) {
+    if (typeof(Storage) !== "undefined") {
+        let totalEstoque = localStorage.getItem("totalEstoque");
+        if (totalEstoque == null) totalEstoque = 0;
+        document.getElementById(idCampo).innerHTML = totalEstoque;
+    }
+    else alert("A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação");
+}
+
+//-----------------------------------------------------------------------------------------------------------
+// Exibe todos os itens do estoque (nome, código e quantidade)
+//-----------------------------------------------------------------------------------------------------------
+function listarEstoque() {
+    if (typeof(Storage) !== "undefined") {
+        let produtos = localStorage.getItem("produtos");
+        document.write("<h1>Estoque:</h1>")
+        if (produtos == null)
+            document.write("<h3>Ainda não há nenhum item no estoque</h3>");
+        else {
+            produtos = JSON.parse(produtos);
+            produtos.forEach(produto => {
+                document.write("<ul>");
+                document.write("<li>Nome do produto: "+produto.nome+"</li>");
+                document.write("<li>Código do produto: "+produto.codigo+"</li>");
+                document.write("<li>Quantidade no estoque: "+produto.quantidade+"</li>");
+                document.write("</ul>");
+            });
+        }
+    } 
+    else alert("Não será possível visualizar o estoque!");    
+}
